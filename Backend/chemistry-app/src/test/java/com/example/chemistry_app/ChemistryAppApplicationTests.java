3886f88799105e729b8fec1e91fec17a8fbd9e64
NaGit09 <nhutanhtran806@gmail.com>
2025-03-19 22:48:14 +0700

    init project

---
+++ Backend/chemistry-app/src/test/java/com/example/chemistry_app/ChemistryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chemistry_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemistryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
